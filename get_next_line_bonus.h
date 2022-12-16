987254a53943136da0cbfa19885a6b702c370010
Mickael Bruzzi <mbruzzi@c3r4s4.42malaga.com>
2022-12-16 12:57:59 +0100

    getnextline

---
+++ get_next_line_bonus.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbruzzi <mbruzzi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/19 13:34:24 by mbruzzi           #+#    #+#             */
+/*   Updated: 2022/10/26 15:03:53 by mbruzzi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+char	*get_next_line(int fd);
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(char *s, int c);
+char	*ft_strcpy(char *dest, char *src);
+size_t	ft_strlen(char *str);
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbruzzi <mbruzzi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/19 13:24:12 by mbruzzi           #+#    #+#             */
+/*   Updated: 2022/10/26 14:00:50 by mbruzzi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+void	ft_bzero(void *s, size_t n)
+{
+	size_t	i;
+	char	*c;
+
+	i = 0;
+	c = s;
+	while (i < n)
+	{
+		c[i] = 0;
+		i++;
+	}
+}
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*created;
+
+	created = malloc(count * size);
+	if (!created)
+		return (NULL);
+	ft_bzero(created, count * size);
+	return (created);
+}
+
+size_t	ft_strlen(char *str)
+{	
+	size_t	c;
+
+	if (!str)
+		return (0);
+	c = 0;
+	while (str[c] != '\0')
+	{
+		c++;
+	}
+	return (c);
+}
+
+char	*ft_strchr(char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	if (s[i] == (char)c)
+		return ((char *)&s[i]);
+	return (NULL);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	int		i;
+	int		len1;
+	int		len2;
+	char	*str;
+
+	len1 = (int)ft_strlen(s1);
+	len2 = (int)ft_strlen(s2);
+	str = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (len1 > i)
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	i = 0;
+	while (len2 > i)
+	{
+		str[len1 + i] = s2[i];
+		i++;
+	}
+	str[len1 + i] = '\0';
+	return (str);
+}
